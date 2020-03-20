bda550e4cd798a051620633c1e01e2be68f28ceb
Jason Strouphauer <jdstroup10@gmail.com>
2020-03-19 19:28:05 -0500

    Commit first round of changes to js file.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+//Global Variables
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var lowerCaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -9,6 +16,83 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+
+function generatePassword() {
+  var passwordSize = prompt("What is your password length? (Choose a length 8-128)");
+  var lowerCase = confirm("Would you like your password to contain lowercase characters?");
+  //var upperCase = confirm("Would you like your password to contain uppercase characters?");
+  //var numeric = confirm("Should your password contain numeric characters?");
+  //var special = confirm("Should your password contain special characters?");
+  var localPassword = "";
+  var localChar = "";
+  //stops working if you go higher than 10
+  alert(passwordSize)
+  if (passwordSize < 8) {
+    alert("Error: Password length must be between 8-128");
+  }
+
+  if (lowerCase) {
+    alert("LOWER");
+      for (i = 0; i < passwordSize; i++ ) {
+          localChar = lowerCaseCharacters[Math.floor(Math.random() * lowerCaseCharacters.length)];
+          localPassword += localChar;
+      }
+  }
+
+  //if (upperCase) {
+   // alert("UPPER");
+  //}
+
+
+  //if (numeric) {
+  //  alert("NUMERIC");
+  //}
+
+
+  //if (special) {
+  //  alert("SPECIAL");
+  //} 
+
+  return localPassword;
+  
+
+
+
+}
+
+
+
+ // var passwordLength = prompt("What is your password length? (Choose a length 8-128)");
+  //var complexity = document.getElementsByName("passwordLength").value;
+
+  //Password values
+  //var values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?";
+
+  //var passcode = "";
+
+  //for(var i = 0; i <= complexity; i++){
+ //   passcode = passcode + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+ // }
+
+  //Put password in text box
+  //document.getElementById("#password").value = passcode;
+
+  
+  
+  
+  //var lowerCase = confirm("Would you like your password to contain lowercase characters?");
+  //var upperCase = confirm("Would you like your password to contain uppercase characters?");
+  //var numeric = confirm("Should your password contain numeric characters?");
+  //var specialCharacters = confirm("Should your password contain special characters?");
+  //alert("Alert")
+
+
+
+  
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
